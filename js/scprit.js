002c7329d65957bd686efa5d63ff142fa178b81b
SarvamFonia <sarvamfonia@gmail.com>
2024-08-30 17:41:47 +0530

    user responsive

--- js/scprit.js
+++ js/scprit.js
@@ -12,30 +12,40 @@ words.forEach((word) => {
   });
 });
 
+
+
 let currentWordIndex = 0;
 let maxWordIndex = words.length - 1;
 words[currentWordIndex].style.opacity = "1";
 
-let rotateText = () => {
+let rotateText = () => { debugger;
   let currentWord = words[currentWordIndex];
   let nextWord =
     currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+  
+  
   // rotate out letters of current word
   Array.from(currentWord.children).forEach((letter, i) => {
     setTimeout(() => {
       letter.className = "letter out";
     }, i * 80);
   });
+
   // reveal and rotate in letters of next word
   nextWord.style.opacity = "1";
   Array.from(nextWord.children).forEach((letter, i) => {
     letter.className = "letter behind";
+    
     setTimeout(() => {
       letter.className = "letter in";
     }, 340 + i * 80);
   });
   currentWordIndex =
     currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+
+    
+  
 };
 
 rotateText();
